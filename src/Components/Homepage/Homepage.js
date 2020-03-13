9b6e351610a1675886833762e4cdc769c36a3eed
Piyush Bendale <thetechnolover7@gmail.com>
2020-03-13 23:50:42 +0530

    asd

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -17,7 +17,7 @@ export default class Homepage extends Component {
 
     let url = `${encodeURIComponent(window.location.origin)}/auth/callback`;
 
-    window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`;
+    window.location = `http://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`;
   }
 
   render() {
